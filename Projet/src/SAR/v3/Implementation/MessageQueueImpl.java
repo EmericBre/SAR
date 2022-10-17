cbe029467f08cbf71ccac72ad3f8f380b0f262fe
EmericBre <emeric.breton31@gmail.com>
2022-10-17 23:00:04 +0200

    Début de modification de l'implem

---
+++ Projet/src/SAR/v3/Implementation/MessageQueueImpl.java
@@ -0,0 +1,101 @@
+package SAR.v3.Implementation;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class MessageQueueImpl extends MessageQueue {
+	
+	private boolean closed;
+	private ChannelImpl channel;
+	private ReentrantLock sendlock;
+	private ReentrantLock receivelock;
+
+	MessageQueueImpl(ChannelImpl channel) {
+		super();
+		this.closed = false;
+		this.channel = channel;
+		this.receivelock = new ReentrantLock();
+		this.sendlock = new ReentrantLock();
+	}
+	
+	void send(byte[] bytes, int offset, int length) {
+		this.sendlock.lock();
+		byte[] messageLength = ByteBuffer.allocate(Integer.BYTES).putInt(length).array();
+		int counter = 0;
+		try {
+			counter = this.channel.write(messageLength, 0, Integer.BYTES);
+			counter += this.channel.write(bytes, 0, length);
+			if (counter != length+Integer.BYTES) {
+				return;
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			this.sendlock.unlock();
+//			e.printStackTrace();
+		}
+	}
+	
+	byte[] receive() {
+		this.receivelock.lock();
+		byte[] bytes = new byte[64];
+		
+		try {
+			int result = this.channel.read(bytes, 0, Integer.BYTES);
+			if (result != Integer.BYTES) {
+				throw new IOException("Impossible to read message length");
+			}
+			int length= (bytes[0]<<24)&0xff000000|
+				       (bytes[1]<<16)&0x00ff0000|
+				       (bytes[2]<< 8)&0x0000ff00|
+				       (bytes[3]<< 0)&0x000000ff;
+			result = this.channel.read(bytes, 4, length);
+			if (result == -1) {
+				return null;
+			}
+			this.receivelock.unlock();
+			return bytes;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			this.receivelock.unlock();
+//			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	void close() {
+		this.closed = true;
+		this.channel.disconnect();
+	}
+	
+	boolean closed() {
+		return this.closed;
+	}
+	
+	boolean getChannelConnect() {
+		return this.channel.disconnected();
+	}
+	
+	ChannelImpl getChannel() {
+		return channel;
+	}
+
+	@Override
+	void setListener(Listener l) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	boolean send(byte[] bytes, Object cookie) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	boolean send(byte[] bytes, int offset, int length, Object cookie) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+}
---
+++ Projet/src/SAR/v3/Implementation/Server.java
@@ -0,0 +1,41 @@
+package SAR.v3.Implementation;
+
+public class Server extends Task {
+	
+	QueueBroker broker;
+	int port;
+	
+	public Server(String name, int port, Manager manager) {
+		this.broker = new QueueBrokerImpl(name, manager);
+		this.port = port;
+	}
+	
+	public void run() {
+		MessageQueue mq;
+		try {
+//			mq = broker.accept(port); // On accepte la connexion
+									
+//			while (!mq.closed()) {
+//				byte[] message = mq.receive();
+//				if (message==null) {
+//					mq.close();
+//					break;
+//				}
+//				
+//				int length= (message[0]<<24)&0xff000000|
+//					       (message[1]<<16)&0x00ff0000|
+//					       (message[2]<< 8)&0x0000ff00|
+//					       (message[3]<< 0)&0x000000ff;
+//				for (int i = 4; i < 4+length; i++) {
+//					System.out.print((char)message[i]);
+//				}
+//				System.out.println();
+//			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+//			e.printStackTrace();
+			return;
+		}
+	}
+
+}
